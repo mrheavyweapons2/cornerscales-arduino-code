c402cd70b5707a069e90f27af917e56e3f633f5f
Jeremiah <jeremiahjnairn@gmail.com>
2025-11-21 16:57:42 -0500

    0.2.0 making lcd's work

--- main/main.ino
+++ main/main.ino
@@ -12,20 +12,29 @@ CONTRIBUTORS:
 #include "HX711.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "include/display.h"
 
 //declare the displays
 LiquidCrystal_I2C lcd_master(0x27, 20, 4); //master display (placed in the middle)
+
 
 //arduino setup
 void setup() {
     Wire.begin();
     //set the serial and declare the scanner active
     Serial.begin(9600);
+    //initialize the LCD's
+    lcd_master.init();
+    lcd_master.backlight();
 }
 
+int hello = 1;
 void loop() {
     //code
-
+    hello +=1;
+    lcd_master.setCursor(1,0);
+    lcd_master.print(hello);
     //set loop interval to 2 seconds
     delay(2000);
+    lcd_master.clear();
 }
